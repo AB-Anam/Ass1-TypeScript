813bc7c61849761091606e35143986218e2d7ac5
abanam <abanam275@gmail.com>
2025-05-08 23:51:39 +0600

    problem 8  solved

--- src/index.ts
+++ src/index.ts
@@ -104,3 +104,18 @@ interface Product {
     else {
         return "Weekday";
     }
+}
+
+
+async function squareAsync(n: number): Promise<number> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (n < 0) {
+          reject(new Error("Negative number not allowed"));
+        } 
+        else {
+          resolve(n * n);
+        }
+      }, 1000);
+    });
+  }
